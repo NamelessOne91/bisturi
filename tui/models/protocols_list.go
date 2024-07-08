92e638f596da052fffa99514dfc60a1e90bfcd88
Paolo Invernizzi <paolo.invernizzi@suse.com>
2024-07-08 18:26:40 +0200

    Use separate models for protocols and network interfaces lists

--- tui/models/protocols_list.go
+++ tui/models/protocols_list.go
@@ -5,6 +5,7 @@ import (
 	"syscall"
 
 	"github.com/charmbracelet/bubbles/list"
+	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 )
 
@@ -20,23 +21,45 @@ func (p protoItem) Description() string { return fmt.Sprintf("Eth type 0x%X", p.
 
 func (p protoItem) FilterValue() string { return p.name }
 
+type selectedProtocolItemMsg protoItem
+
 type protocolsListModel struct {
 	l list.Model
+}
+
+func (m protocolsListModel) Init() tea.Cmd {
+	return nil
+}
+
+func (m protocolsListModel) Update(msg tea.Msg) (protocolsListModel, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "enter":
+			p, ok := m.l.SelectedItem().(protoItem)
+			if ok {
+				return m, func() tea.Msg {
+					return selectedProtocolItemMsg(p)
+				}
+			}
+		case "q", "ctrl+c":
+			return m, tea.Quit
+		}
+	}
+	var cmd tea.Cmd
+	m.l, cmd = m.l.Update(msg)
+	return m, cmd
+}
+
+func (m protocolsListModel) View() string {
+	return m.l.View()
 }
 
 func newProtocolsListModel(width, height int) protocolsListModel {
 	protoDelegate := list.NewDefaultDelegate()
 	protoDelegate.Styles.SelectedTitle = lipgloss.NewStyle().Foreground(lipgloss.Color("#00cc99"))
 
-	items := []list.Item{}
-	protoList := list.New(items, protoDelegate, width, height)
-	protoList.Title = "Select a Network Protocol"
-	protoList.Styles.Title = lipgloss.NewStyle().Foreground(lipgloss.Color("#00cc99")).Blink(true).Bold(true)
-	protoList.SetShowStatusBar(false)
-	protoList.SetFilteringEnabled(false)
-	protoList.SetShowHelp(true)
-
-	protoList.SetItems([]list.Item{
+	items := []list.Item{
 		protoItem{name: "all", ethType: syscall.ETH_P_ALL},
 		protoItem{name: "arp", ethType: syscall.ETH_P_ARP},
 		protoItem{name: "ip", ethType: syscall.ETH_P_IP},
@@ -46,7 +69,13 @@ func newProtocolsListModel(width, height int) protocolsListModel {
 		protoItem{name: "udp6", ethType: syscall.ETH_P_IPV6},
 		protoItem{name: "tcp", ethType: syscall.ETH_P_IP},
 		protoItem{name: "tcp6", ethType: syscall.ETH_P_IPV6},
-	})
+	}
+	protoList := list.New(items, protoDelegate, width, height)
+	protoList.Title = "Select a Network Protocol"
+	protoList.Styles.Title = lipgloss.NewStyle().Foreground(lipgloss.Color("#00cc99")).Blink(true).Bold(true)
+	protoList.SetShowStatusBar(false)
+	protoList.SetFilteringEnabled(false)
+	protoList.SetShowHelp(true)
 
 	return protocolsListModel{l: protoList}
 }
