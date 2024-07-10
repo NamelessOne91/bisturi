1b1defe96d38a2afe5e67259c8ce2fa848a68463
Paolo Invernizzi <paolo.invernizzi@suse.com>
2024-07-11 00:36:51 +0200

    Log to file when debugging is enabled

--- cmd/bisturi/main.go
+++ cmd/bisturi/main.go
@@ -2,12 +2,21 @@ package main
 
 import (
 	"log"
+	"os"
 
 	models "github.com/NamelessOne91/bisturi/tui/models"
 	tea "github.com/charmbracelet/bubbletea"
 )
 
 func main() {
+	if len(os.Getenv("BISTURI_DEBUG")) > 0 {
+		f, err := tea.LogToFile("bisturi_debug.log", "debug")
+		if err != nil {
+			log.Fatal("Failed to setup logging:", err)
+		}
+		defer f.Close()
+	}
+
 	p := tea.NewProgram(models.NewBisturiModel(), tea.WithAltScreen())
 	if _, err := p.Run(); err != nil {
 		log.Fatal("Error running program:", err)
