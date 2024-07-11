64b3fc45658e5f5eca3ce1a220811ae4cbd20b1e
Paolo Invernizzi <paolo.invernizzi@suse.com>
2024-07-11 19:20:42 +0200

    Clear screen at startup

--- cmd/bisturi/main.go
+++ cmd/bisturi/main.go
@@ -3,10 +3,17 @@ package main
 import (
 	"log"
 	"os"
+	"os/exec"
 
 	models "github.com/NamelessOne91/bisturi/tui/models"
 	tea "github.com/charmbracelet/bubbletea"
 )
+
+func clearScreen() error {
+	cmd := exec.Command("clear") // On Windows use exec.Command("cmd", "/c", "cls")
+	cmd.Stdout = os.Stdout
+	return cmd.Run()
+}
 
 func main() {
 	if len(os.Getenv("BISTURI_DEBUG")) > 0 {
@@ -15,6 +22,10 @@ func main() {
 			log.Fatal("Failed to setup logging:", err)
 		}
 		defer f.Close()
+	}
+
+	if err := clearScreen(); err != nil {
+		log.Fatal("Failed to clear the screen: ", err)
 	}
 
 	p := tea.NewProgram(models.NewBisturiModel(), tea.WithAltScreen())
